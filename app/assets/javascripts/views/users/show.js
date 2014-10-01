e35142130189b15c1334d6738f199abd7338d810
Anna Petry <hello@annapetry.com>
2014-10-01 16:55:23 -0700

    Add following user images and links.

--- app/assets/javascripts/views/users/show.js
+++ app/assets/javascripts/views/users/show.js
@@ -2,19 +2,14 @@ Pintrospective.Views.UserShow = Backbone.CompositeView.extend({
   template: JST["users/show"],
 
   initialize: function (options) {
-    debugger
     this.subview = options.subview;
     this.htmlEl = options.htmlEl;
     this.listenTo(this.model, "sync", this.render);
     this.createSubviews();  
- 
-    this.$('.large-section-divider a').removeClass('active');
-    
-    var frag = '/#/' + Backbone.history.fragment;
-    
-    this.$("a[href='frag']").addClass('active');
+
+
   },
-  
+
   events: {
     "click button.unfollowed": "followUser",
     "click button.followed": "unfollowUser",
@@ -29,12 +24,12 @@ Pintrospective.Views.UserShow = Backbone.CompositeView.extend({
     });
     this.addSubviewBefore(this.htmlEl, view);
   },
-  
+
   createSubview: function () {
     var $filePickerInput = this.$('input[type=filepicker]');
     filepicker.constructWidget($filePickerInput[0]);
   },
-  
+
   render: function () {
     var renderedContent = this.template({ 
       user: this.model,
@@ -42,9 +37,9 @@ Pintrospective.Views.UserShow = Backbone.CompositeView.extend({
     });
     this.$el.html(renderedContent);
     this.attachSubviewsBefore();
-    
+
     this.createSubview();
-    
+
     $('#edit-user-profile').popover({
       html: true,
       placement: 'bottom'
@@ -53,26 +48,51 @@ Pintrospective.Views.UserShow = Backbone.CompositeView.extend({
     this.$editUserModal = this.$('#editUserModal');
     this.$followToggle = this.$('#user-follow-button');
     this.addToggle();
+
+    this.$('.large-section-divider a').removeClass('active');
+    var frag = '/#/' + Backbone.history.fragment;
+    this.$("a[href='" + frag + "']").addClass('active');
+
+    this.followerBox();
+
     this.stickit();
-    
+
     return this;  
   },
-  
+
+  followerBox: function () {
+    var that = this;
+    if (that.model.get('following')) { 
+      _.times(3, function (x) { 
+        if (that.model.get('following')[x]) { 
+          var $a = $('<a class="tiny-follower"></a>')
+          var $img = $('<img>');
+          var $p = $('<p></p>');
+          $img.attr('src', that.model.get('following')[0].url);
+          $a.attr('href', '/#/users/' + that.model.get('following')[0].id);
+          $p.text(that.model.get('following')[0].username);
+          
+          $a.append($img);
+          $a.append($p);
+          that.$('.side-note').append($a);
+    }})}
+  },
+
   addToggle: function () {
     this.followState = this.$followToggle.data('follow-state');
     if (this.followState == "followed") {
       this.$followToggle.addClass('followed');
       this.$followToggle.prop("disabled", false);
       this.$followToggle.html("Unfollow User");
-      
+
     } else if (this.followState == "unfollowed") {
       this.$followToggle.addClass('unfollowed');
       this.$followToggle.prop("disabled", false);
       this.$followToggle.html("Follow User");
     }
   },
-  
-  
+
+
   followUser: function (event) {
     event.preventDefault();
     var followee_user = new Pintrospective.Models.Follow({
@@ -90,14 +110,14 @@ Pintrospective.Views.UserShow = Backbone.CompositeView.extend({
       }
     });
   },
-  
+
   unfollowUser: function (event) {
     event.preventDefault();
     if (this.model.followers().length > 0) {
       var follow = this.model.followers().first();
-      
+
       var that = this;
-      
+
       follow.destroy({
         success: function () {
           that.$followToggle.data("follow-state", 'unfollowed');
@@ -108,12 +128,12 @@ Pintrospective.Views.UserShow = Backbone.CompositeView.extend({
       });
     }
   },
-  
+
   editUser: function (event) {
     event.preventDefault();
     var formData = $(event.currentTarget).serializeJSON();
     this.$editUserModal.modal('hide');
-  
+
     var that = this;
     this.$editUserModal.one('hidden.bs.modal', function (){
       that.model.set({
@@ -124,16 +144,16 @@ Pintrospective.Views.UserShow = Backbone.CompositeView.extend({
           url: formData.image.url         
         }
       });
-    var image_id = that.model.get('image_id');  
-    
+      var image_id = that.model.get('image_id');  
+
       that.model.save();
     });
   },
-  
+
   removeBoard: function (boardSubView) {
     this.removeSubview('#board-items', boardSubView);
   },
-  
+
   addActive: function (event) {
     debugger
     $('large-section-divider a').removeClass('active');
