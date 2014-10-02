61bd54a0cfe8311ff324241a9b0a05aaf14f9d32
Anna Petry <hello@annapetry.com>
2014-10-01 19:21:32 -0700

    Sign up page works like a dream.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,29 @@
 class UsersController < ApplicationController
+   wrap_parameters :user, include: [:username, :location, :description, :image_attributes]
+
   def new; end
 
   def create
     @user = User.new(user_params)
 
+    @user.update_attributes({
+      image_attributes: {
+        url: params[:image][:url],
+        imageable_type: "User"
+      }
+    })
+
     if @user.save
       login!(@user)
       redirect_to root_url
     else
-      flash.now[:errors] = @user.errors.full_messages
+      flash[:errors] = @user.errors.full_messages
       render :new
     end
   end
 
   private
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password, :description, :location, :image_attributes => [:url, :imageable_id, :imageable_type])
   end
 end
