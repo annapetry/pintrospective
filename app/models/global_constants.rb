1a1930463894af2d1bdc2eef4c65f9815342932d
Anna Petry <hello@annapetry.com>
2014-10-02 17:02:18 -0700

    Add Categories

--- app/models/global_constants.rb
+++ app/models/global_constants.rb
@@ -1,3 +1,14 @@
 module GlobalConstants
-  CATEGORIES = ['Impressionism', 'Cubism', 'Post-Impressionism', 'Modern', 'Post-Modern', 'Other']
+  CATEGORIES = [
+    'Abstract',
+    'Cubism',
+    'Impressionism',
+    'Modern',
+    'Other',
+    'Post-Impressionism',
+    'Post-Modern',
+    'Realism',
+    'Self-Portrait',
+    'Surrealism'
+  ]
 end
