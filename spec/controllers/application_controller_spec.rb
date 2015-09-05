29acb6ddb1f7d70321064db6ee8dbb1bc360d732
Anna Petry <hello@annapetry.com>
2015-09-05 09:38:18 -0700

    add application controller specs

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe ApplicationController do
+  let(:user) { FactoryGirl.create :user }
+
+  before { subject.login!(user) }
+
+  describe "#current_user" do
+    it "returns the current user" do
+      expect(subject.current_user).to eq user
+    end
+  end
+
+  describe "#login!" do
+    it "sets current_user to given user" do
+      expect(subject.current_user).to eq user
+    end
+    it "sets the session_token to user.session_token" do
+      expect(session[:session_token]).to eq user.session_token
+    end
+  end
+
+  describe "#require_user" do
+    context "current_user is not set" do
+      before { current_user = nil }
+      it "redirects to new_session_url"
+    end
+    context "current_user is set" do
+      it "returns nil" 
+    end
+  end
+end
